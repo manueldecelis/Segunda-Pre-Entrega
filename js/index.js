084bab66ad82f413885103d74450975550f5a644
Manuel De Celis <manueldecelis@gmail.com>
2024-02-05 20:03:44 -0300

    Segunda Pre Entrega

---
+++ js/index.js
@@ -0,0 +1,222 @@
+const contenedorProductos = document.getElementById('container-products') 
+const modal = document.getElementById('ventana-modal')
+const carrito = document.getElementById('carrito')
+const totalCarrito = document.getElementById('total')
+const btnCerrar = document.getElementsByClassName('close')[0]
+const contenedorCarrito = document.querySelector('.modal-body')
+const iconMenu = document.getElementById('icon-menu')
+const contenedorProductosCarrito = document.querySelector('.contenedor-carrito')
+const cantidadProductos = document.querySelector('.count-products')
+const vaciarCarrito = document.querySelector('#vaciar-carrito')
+let carritoCompras = [] 
+
+class Producto { 
+    constructor(imagen, nombre, precio, id){
+        this.imagen = imagen;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
+        this.cantidad = 1; 
+        this.subtotal = 0; 
+    }
+
+    obtenerTotal(){ 
+        this.subtotal = this.precio * this.cantidad;
+    }
+}
+
+cargarEventos(); 
+
+function cargarEventos(){
+    iconMenu.addEventListener('click', showMenu) 
+
+    document.addEventListener('DOMContentLoaded', ()=>{ 
+        renderizarProductos(); 
+        cargarCarritoLocalStorage();
+        mostrarProductosCarrito();
+    });
+    
+    
+    contenedorProductos.addEventListener('click', agregarProducto); 
+    contenedorCarrito.addEventListener('click', eliminarProducto);
+
+    carrito.onclick = function(){ 
+        modal.style.display = 'block';
+    };
+
+    btnCerrar.onclick = function(){ 
+        ocultarModal();
+    };
+    
+    window.onclick = function(event){ 
+        if (event.target == modal){ 
+            ocultarModal();
+        }
+    };
+
+    vaciarCarrito.onclick = function(){ 
+        vaciarCarro();
+    };
+
+}
+
+function ocultarModal(){ 
+    modal.style.display = 'none';
+}
+
+function cargarCarritoLocalStorage(){
+    carritoCompras = JSON.parse(localStorage.getItem('productosLS')) || []; 
+}
+
+function eliminarProducto(e){
+    if (e.target.classList.contains('eliminar-producto')){ 
+        const productoId = parseInt(e.target.getAttribute('id')); 
+        carritoCompras = carritoCompras.filter((producto) => producto.id !== productoId); 
+        
+        guardarProductosLocalStorage();
+        console.log(carritoCompras);
+        mostrarProductosCarrito();
+    }
+}
+
+function agregarProducto(e){ 
+    e.preventDefault(); 
+
+    if (e.target.classList.contains('agregar-carrito')){ 
+        const productoAgregado = e.target.parentElement; 
+        
+        leerDatosProducto(productoAgregado);
+    }
+}
+
+function leerDatosProducto(producto){ 
+    let imagen, nombre, precio, id; 
+    
+    imagen = producto.querySelector('img').src; 
+    nombre = producto.querySelector('h4').textContent; 
+    precio = Number(producto.querySelector('p').textContent.replace('$','')); 
+    id = parseInt(producto.querySelector('a').getAttribute('id')); 
+
+    
+    const datosProducto = new Producto(imagen, nombre, precio, id);
+    datosProducto.obtenerTotal(); 
+
+    agregarAlCarrito(datosProducto);
+}
+
+function agregarAlCarrito(productoAgregar){ 
+
+    const existeEnCarrito = carritoCompras.some((producto) => producto.id === productoAgregar.id); 
+
+    if (existeEnCarrito){ 
+        
+        const productos = carritoCompras.map((producto) => { 
+            if (producto.id === productoAgregar.id){ 
+                producto.cantidad++;
+                producto.subtotal = producto.precio * producto.cantidad;
+
+                
+                return producto;
+            }else{
+                
+                return producto;
+            }
+        });
+
+        carritoCompras = productos; 
+    }else{
+        carritoCompras.push(productoAgregar);
+    }
+    
+    guardarProductosLocalStorage();
+    console.log(carritoCompras);
+    mostrarProductosCarrito();
+}
+
+function mostrarProductosCarrito(){ 
+    limpiarHTML(); 
+
+    carritoCompras.forEach((producto) => { 
+        const { imagen, nombre, precio, cantidad, subtotal, id } = producto; 
+        
+        const div = document.createElement('div'); 
+        div.classList.add('contenedor-producto'); 
+        div.innerHTML = `
+            <img src="${imagen}" width="100">
+            <p>${nombre}</p>
+            <p>${precio}</p>
+            <p>${cantidad}</p>
+            <p>${subtotal}</p>
+            <a href="#" class="eliminar-producto" id="${id}"> X </a>
+        `; 
+
+        contenedorCarrito.appendChild(div); 
+    });
+
+    mostrarCantidadProductos();
+    calcularTotal();
+}
+
+function mostrarCantidadProductos(){ 
+    let contarProductos; 
+    
+    if (carritoCompras.length > 0){ 
+        contenedorProductosCarrito.style.display = 'flex';
+        contenedorProductosCarrito.style.alignItems = 'center';
+        cantidadProductos.style.display = 'flex';
+        contarProductos = carritoCompras.reduce((cantidad, producto) => cantidad + producto.cantidad, 0); 
+        cantidadProductos.innerText = `${contarProductos}`;
+    }else{ 
+        contenedorProductosCarrito.style.display = 'block';
+        cantidadProductos.style.display = 'none';
+    }
+}
+
+function calcularTotal(){ 
+    let total = carritoCompras.reduce((sumatotal, producto) => sumatotal + producto.subtotal, 0);
+    totalCarrito.innerHTML = `Total a Pagar: $ ${total}`; 
+}
+
+function limpiarHTML(){ 
+    while (contenedorCarrito.firstChild){ 
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild); 
+    }
+}
+
+function guardarProductosLocalStorage(){
+    localStorage.setItem('productosLS', JSON.stringify(carritoCompras));
+}
+
+function renderizarProductos(){ 
+    productos.forEach((producto) => { 
+        const divCard = document.createElement('div');
+        divCard.classList.add('card') 
+        divCard.innerHTML += `        
+        <img src="./img/${producto.img}" alt="${producto.nombre}"/>
+        <h4>${producto.nombre}</h4>
+        <p><strong>$ ${producto.precio}</strong></p>
+        <a id=${producto.id} class="boton agregar-carrito" href="#">Agregar</a>
+        `; 
+
+        contenedorProductos.appendChild(divCard); 
+    });
+}
+
+function showMenu(){ 
+    let navbar = document.getElementById('barra-navegacion')
+    
+    if (navbar.className === 'barra-navegacion'){
+        navbar.className += ' responsive';
+    }else{
+        navbar.className = 'barra-navegacion'
+    }
+}
+
+function vaciarCarro(){
+    localStorage.clear()
+    limpiarHTML();
+    totalCarrito.innerHTML = `Total a Pagar: $ 0`;
+    contenedorProductosCarrito.style.display = 'block';
+    cantidadProductos.style.display = 'none';
+}
+
---
+++ js/productos-json.js
@@ -0,0 +1,51 @@
+const productos = [
+    {
+        id:1,
+        nombre: 'SENSODYNE DENT BLAN E/FREX 90',
+        precio: 2950,
+        img:'SENSODYNE.jpg',
+        stock: 5,
+        descripcion: 'Dentifrico refrescante'
+    },
+    {
+        id:2,
+        nombre: 'ESTRELLA ALGODON SUPER X100GR',
+        precio: 1700,
+        img:'ESTRELLA.png',
+        stock: 10,
+        descripcion: 'Algodon super suave'
+    },
+    {
+        id:3,
+        nombre: 'CURITAS APOS TELA X10U',
+        precio: 600,
+        img:'CURITAS.jpg',
+        stock: 4,
+        descripcion: 'Curitas de tela elastica, comodas y flexibles'
+    },
+    {
+        id:4,
+        nombre: 'Dermaglos Crema Con Vitamina A - 50 gr',
+        precio: 5377,
+        img:'CREMA-HIDRATANTE.jpg',
+        stock: 5,
+        descripcion: 'Está demostrado que aplicar diariamente una crema rica en vitamina A mejora la suavidad de la piel, la elasticidad y la pigmentación.'
+    },
+    {
+        id:5,
+        nombre: 'Shampoo Dove Regeneración Extrema -750ml',
+        precio: 5644,
+        img:'SHAMPOO.jpg',
+        stock: 10,
+        descripcion: 'Shampoo Dove Regeneración Extrema -750ml'
+    },
+    {
+        id:6,
+        nombre: 'Protector Solar Dermaglós FPS 50 x 250 ml',
+        precio: 8072,
+        img:'PROTECTOR-SOLAR.JPG',
+        stock: 4,
+        descripcion: 'Protector Dermaglós Solar FPS 50 y UVA UVB con Vitamina E, resistente al agua x 250 ml.'
+    }
+]
+
